439dc5ddf2cd35077f6ff694b54e1b3e42521698
balujonnala <45555897+jonnalasaikrishna@users.noreply.github.com>
2022-05-20 18:07:54 +0530

    Delete AngularUI/AirlineUI directory

--- AngularUI/AirlineUI/src/app/models/RegisterData.ts
+++
@@ -1,9 +0,0 @@
-export class RegisterData{
-    firstName:string='';
-    lastName:string='';
-    mobileNum:string='';
-    email:string='';
-    address:string='';
-    dateOfBirth:string='';
-    password:string='';
-  }
--- AngularUI/AirlineUI/src/app/models/UserData.ts
+++
@@ -1,4 +0,0 @@
-export class UserData{
-  userName:string='';  
-  password:string='';
-}
